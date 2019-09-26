4a2079716ed5060d3d64672c54bf6d55c39d4224
Task2 <sadat>
2019-09-26 19:29:48 +0330

    add log off menu in toolbar and complete fragment layout todo(recyclerview + floating Button )

--- app/src/main/java/com/example/myannuallytasks/Done_Fragment.java
+++ app/src/main/java/com/example/myannuallytasks/Done_Fragment.java
@@ -1,11 +1,15 @@
 package com.example.myannuallytasks;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
@@ -29,11 +33,40 @@ public static Done_Fragment newInstance() {
     return fragment;
 }
 
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+
+        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.taskmenu, menu);
+
+        MenuItem subtitleItem = menu.findItem(R.id.id_LogOff);
+    }
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        setHasOptionsMenu(true);
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_done_, container, false);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.id_LogOff:
+                Intent intent =new Intent(getActivity(),First_Activity.class);
+                startActivity(intent);
+                return true;
+            default:
+
+
+
+
+                return super.onOptionsItemSelected(item);
+
+
+        }
     }
 
 }
--- app/src/main/java/com/example/myannuallytasks/To_Do_Fragment.java
+++ app/src/main/java/com/example/myannuallytasks/To_Do_Fragment.java
@@ -1,11 +1,16 @@
 package com.example.myannuallytasks;
 
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
@@ -33,13 +38,39 @@ public class To_Do_Fragment extends Fragment {
 
 
     @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+
+        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.taskmenu, menu);
+
+        MenuItem subtitleItem = menu.findItem(R.id.id_LogOff);
+
+    }
+
+    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+        setHasOptionsMenu(true);
         return inflater.inflate(R.layout.fragment_to__do_, container, false);
 
 
     }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.id_LogOff:
+             Intent intent =new Intent(getActivity(),First_Activity.class);
+             startActivity(intent);
+                return true;
+            default:
 
-}
+
+
+
+        return super.onOptionsItemSelected(item);
+
+
+    }
+}}
